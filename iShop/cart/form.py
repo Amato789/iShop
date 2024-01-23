4cf71ff5de9092dd6ccd1a8e3d4d8a6af2376d15
Amato789 <maximsidorchuk@gmail.com>
2024-01-23 20:53:25 +0200

    refactoring

--- iShop/cart/form.py
+++ iShop/cart/form.py
@@ -4,5 +4,5 @@ PRODUCT_QUANTITY_CHOICES = 1
 
 
 class CartAddProductForm(forms.Form):
-    quantity = forms.IntegerField(min_value=1)
+    quantity = forms.IntegerField(min_value=1, initial=1)
     override = forms.BooleanField(required=False, initial=False, widget=forms.HiddenInput)
